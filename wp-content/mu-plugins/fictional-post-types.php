7347391ebafdb73b9ca2353167ee3300914e7f87
Steven Loar <sjl7909@gmail.com>
2019-01-07 10:26:13 -0500

    Work through campus post types

--- wp-content/mu-plugins/fictional-post-types.php
+++ wp-content/mu-plugins/fictional-post-types.php
@@ -2,6 +2,22 @@
 
 function fictional_post_types() {
 
+  // Campus Post type
+  register_post_type('campus', array(
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'campuses'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Campuses',
+      'add_new_item' => 'Add New Campus',
+      'edit_item' => 'Edit Campus',
+      'all_items' => 'All Campuses',
+      'singular_name' => 'Campus'
+    ),
+    'menu_icon' => 'dashicons-location-alt'
+  ));
+
   // Event Post Types
   register_post_type('event', array(
     'supports' => array('title', 'editor', 'excerpt'),
@@ -33,6 +49,21 @@ function fictional_post_types() {
     ),
     'menu_icon' => 'dashicons-awards'
   ));
+
+  // Professor Post Type
+  register_post_type('professor', array(
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));
+
 }
 
 add_action('init', 'fictional_post_types');
